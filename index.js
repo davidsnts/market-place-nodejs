4ba4c3a2eb636a15706b607acd4fe6bb19c89314
davidsnts <david.santos@delage.com.br>
2025-07-16 11:47:59 -0300

    Inciando o projeto

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send({
+        message: "Bem vindo ao nosso market-place"
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Rodando na porta http://localhost:${PORT} `);
+})
